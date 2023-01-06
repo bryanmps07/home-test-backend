320b3887ed328c0f24c5514dcaac09a7af9ae7b9
Bryan Pena <bryan.m.p.s_07@live.com>
2023-01-06 21:47:27 +0000

    added interfaces

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,8 @@
 import { Controller, Get, Param } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Commit } from './interfaces/commit.interface';
+import { Observable } from 'rxjs';
+import { AxiosResponse } from 'axios';
 
 @Controller()
 export class AppController {
@@ -9,7 +12,7 @@ export class AppController {
   getCommits(
     @Param('username') username: string, 
     @Param('repository') repository: string
-  ): any {  
+  ): Observable<AxiosResponse<Commit>> {  
     return this.appService.getCommits(username, repository);
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -2,6 +2,7 @@ import { HttpService } from '@nestjs/axios';
 import { Injectable, InternalServerErrorException } from '@nestjs/common';
 import { AxiosResponse } from 'axios';
 import { Observable, map } from 'rxjs';
+import { Commit } from './interfaces/commit.interface';
 
 const gitHubUrl = "https://api.github.com/repos/";
 
@@ -11,13 +12,18 @@ export class AppService {
   constructor(private httpService: HttpService) {}
 
   
-   getCommits(username: string, repository: string): Observable<AxiosResponse<any>> {
+   getCommits(username: string, repository: string): Observable<AxiosResponse<Commit>> {
     const commitUrl = `${gitHubUrl}${username}/${repository}/commits`;
     
     try {
       const response =  this.httpService.get(commitUrl)
       .pipe( 
-        map((response) => response.data)
+        map((response) => {
+          return response.data.map(({commit}) => {
+            const {author, committer,message, tree} = commit;
+            return {author, committer,message,tree};
+          })
+        })
       );
       return response;
       
