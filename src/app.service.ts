a40aaf3fe473c3dda329be307be227c3ea436476
Bryan Pena <bryan.m.p.s_07@live.com>
2023-01-07 01:05:00 +0000

    added commets

--- src/app.service.ts
+++ src/app.service.ts
@@ -4,32 +4,33 @@ import { AxiosResponse } from 'axios';
 import { Observable, map } from 'rxjs';
 import { Commit } from './interfaces/commit.interface';
 
+// URL to get the commits
 const gitHubUrl = "https://api.github.com/repos/";
 
 @Injectable()
 export class AppService {
 
-  constructor(private httpService: HttpService) {}
+  constructor(private httpService: HttpService) { }
 
-  
-   getCommits(username: string, repository: string): Observable<AxiosResponse<Commit>> {
+  //Method to get commits from the repository
+  getCommits(username: string, repository: string): Observable<AxiosResponse<Commit>> {
     const commitUrl = `${gitHubUrl}${username}/${repository}/commits`;
-    
+
     try {
-      const response =  this.httpService.get(commitUrl)
-      .pipe( 
-        map((response) => {
-          return response.data.map(({commit, html_url}) => {
-            const {author, committer, message, tree} = commit;
-            return {author, committer, message, tree, html_url};
+      const response = this.httpService.get(commitUrl)
+        .pipe(
+          map((response) => {
+            // Only getting the object commit and the url to see the changes
+            return response.data.map(({ commit, html_url }) => {
+              const { author, committer, message, tree } = commit;
+              return { author, committer, message, tree, html_url };
+            })
           })
-        })
-      );
+        );
       return response;
-      
     } catch (error) {
       throw new InternalServerErrorException();
-    }    
+    }
   }
 
 }
--- src/interfaces/commit.interface.ts
+++ src/interfaces/commit.interface.ts
@@ -1,24 +1,26 @@
+// Interfaces for the GitHub API
+
 export interface Commit {
-    author: Author
-    committer: Committer
-    message: string
-    tree: Tree,
-    html_url: string
-  }
-  
-  export interface Author {
-    name: string
-    email: string
-    date: string
-  }
-  
-  export interface Committer {
-    name: string
-    email: string
-    date: string
-  }
-  
-  export interface Tree {
-    sha: string
-    url: string
-  }
+  author: Author
+  committer: Committer
+  message: string
+  tree: Tree,
+  html_url: string
+}
+
+export interface Author {
+  name: string
+  email: string
+  date: string
+}
+
+export interface Committer {
+  name: string
+  email: string
+  date: string
+}
+
+export interface Tree {
+  sha: string
+  url: string
+}
